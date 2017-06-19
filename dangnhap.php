fcd553f886504436b69fb6d71357b61c31327aa3
BinhHX <hoangbinhnt1996@gmail.com>
2017-06-20 05:08:26 +0700

    luu tai khoan hien tai

---
+++ dangnhap.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<body>
+	<div class="col-md-6" style="margin-left: 300px;margin-top: 150px; background-color: yellow">
+		<span class="text-center"><h1>Đăng Nhập Hệ Thống</h1></span>
+		<form action="showImf.php" method="post">
+			<div class="col-md-12">
+				<span class="col-md-4" style="padding-right:0px"><h3>Tên Tài Khoản:</h3></span>
+				<input class="col-md-6" style="padding-left:0px; margin-top: 20px" type="text" name="acc">
+			</div>
+			<div class="col-md-12">
+				<span class="col-md-4" style="padding-right:0px"><h3>Mật Khẩu:</h3></span>
+				<input class="col-md-6" style="padding-left:0px; margin-top: 20px" type="text" name="pass">
+			</div>
+			<button type="submit" class="col-md-3 btn btn-primary" style="margin-left: 30px; margin-bottom: 20px" >Đăng Nhập</button>
+		</form>
+	</div>
+</html>
--- showImf.php
+++ showImf.php
@@ -1,29 +1,40 @@
+<?php
+	$cookie_name = $_COOKIE["acc"]; 
+	$cookie_pass = $_COOKIE["pass"]; 
+?>
 <html>
 <body>
 <?php 
-	$ac = $_POST["acount"]; 
-	$pss = $_POST["password"];
-	
-	$servername = "localhost";
-	$username = "root";
-	$password = "";
-	$dbname = "qlnhanvien";
 
-	// Create connection
-	$conn = new mysqli($servername, $username, $password, $dbname);
-	// Check connection
-	if ($conn->connect_error) {
-		die("Connection failed: " . $conn->connect_error);
-	} 
-	$sql = "SELECT * FROM `account` WHERE name ='$ac' && password = '$pss';";
-	$result = $conn->query($sql);
-	
-	if ($result->num_rows == 0) {
-		echo "user not found";
-	} else {
-		echo "<h4>Xin Chao :</h4>".$ac;
+	if(isset($_POST["acc"])) {
+		$cookie_name = $_POST["acc"]; 
+		setcookie('acc', $cookie_name, time() + (86400 * 3), "/");
+		$cookie_pass = $_POST["pass"]; 
+		setcookie('pass',$cookie_pass, time() + (86400 * 3), "/");
+		
+		$servername = "localhost";
+		$username = "root";
+		$password = "";
+		$dbname = "qlnhanvien";
+
+		// Create connection
+		$conn = new mysqli($servername, $username, $password, $dbname);
+		// Check connection
+		if ($conn->connect_error) {
+			die("Connection failed: " . $conn->connect_error);
+		} 
+		$sql = "SELECT * FROM `account` WHERE name ='$cookie_name' && password = '$cookie_pass';";
+		$result = $conn->query($sql);
+		
+		if ($result->num_rows == 0) {
+			echo "user not found";
+		} else {
+			echo "<h4>Xin Chao :</h4>".$cookie_name;
+		}
+		$conn->close();
+	}else{
+		echo "<h4>Xin Chao :</h4>".$cookie_name;
 	}
-	$conn->close();
 ?>
 </body>
 </html>
